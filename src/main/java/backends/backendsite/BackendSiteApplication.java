358cb55da73377a9b436abc86a18a6f76dfb4f90
Ivan <uvanes21@yandex.ru>
2022-10-02 07:44:07 +0300

    Initial commit

---
+++ src/main/java/backends/backendsite/BackendSiteApplication.java
@@ -0,0 +1,13 @@
+package backends.backendsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backends/backendsite/BackendSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package backends.backendsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
